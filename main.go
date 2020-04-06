6404865aa4d424d349b30f8ec2a80fb6acf425c6
Domanat <maxim.tomilko@yandex.ru>
2020-04-06 16:41:39 +0300

    updated folders and new names

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("begin.html"))
+
+	tmpl.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("regist.html"))
+
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", welcome)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/login", login)
+
+	fmt.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
+}
