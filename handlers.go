5413433dd27d19239cbff0acf16ceb504a5188d6
Domanat <maxim.tomilko@yandex.ru>
2020-04-19 23:27:13 +0300

    Delete handler of home page

--- handlers.go
+++ handlers.go
@@ -6,9 +6,6 @@ import (
 	"html/template"
 	"net/http"
 	"regexp"
-	"sort"
-	"strings"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -134,83 +131,4 @@ func thanks(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", http.StatusPermanentRedirect)
 	}
 	tmpl.Execute(w, nil)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("home.html"))
-
-	session, _ := store.Get(r, "UserCookie")
-
-	//if user is authorised, read his username and put it in html
-	if auth, ok := session.Values["Authenticated"].(bool); !auth || !ok {
-		errPage := template.Must(template.ParseFiles("error.html"))
-
-		err := struct {
-			Message string
-		}{
-			Message: "You need to authorise",
-		}
-
-		errPage.Execute(w, err)
-		return
-	}
-
-	user := User{
-		Username: session.Values["Username"].(string),
-		Email:    session.Values["Email"].(string),
-		Password: session.Values["Password"].(string),
-		ID:       session.Values["ID"].(string),
-	}
-
-	if r.FormValue("PostButton") == "Send" {
-
-		timeOfPost := time.Now().String()
-
-		//add post to the database
-		guid := xid.New()
-
-		text := r.FormValue("text")
-		text = strings.ReplaceAll(text, "'", "''")
-
-		query := fmt.Sprintf("INSERT INTO posts(id, text, owner, likes, post_at) VALUES('%s', '%s', '%s', 0, '%s')", guid.String(), text, user.Username, timeOfPost[:19])
-
-		rows, err := db.Query(query)
-		if err != nil {
-			panic(err)
-		}
-
-		defer rows.Close()
-
-	}
-
-	//-----Search all post of logged in user's------------
-	query := fmt.Sprintf("SELECT * FROM posts WHERE owner = '%s'", user.Username)
-	rows, err := db.Query(query)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	var posts []Post
-	for rows.Next() {
-		var post Post
-
-		err := rows.Scan(&post.ID, &post.Text, &post.Owner, &post.Likes, &post.TimeOfPost)
-		if err != nil {
-			panic(err)
-		}
-		posts = append(posts, post)
-	}
-
-	//output them regarding to the date so reverse the posts
-	sort.SliceStable(posts, func(i, j int) bool {
-		return posts[i].TimeOfPost > posts[j].TimeOfPost
-	})
-
-	PostWithOwner := PostsOfUser{
-		Owner: user,
-		Posts: posts,
-	}
-
-	tmpl.Execute(w, PostWithOwner)
 }
